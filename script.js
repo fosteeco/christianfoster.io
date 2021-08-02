83fe9f38085713d4e103c5f60bfb2f7d96ce1ec6
Christian Foster <fosterchris000@gmail.com>
2021-08-02 16:53:20 -0400

    Fixed space between terminal dollar

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var headerH1 = document.querySelector('header h1.name');
-var afterH1 = document.querySelector('header h1::after');
-console.log(afterH1); 
-console.log(headerH1);
-const name = "Christian Foster"
-for(let i =0; i < name.length; i++){
-    setDelay(i,name)
+const headerH1 = document.querySelector("header h1.name");
+const afterH1 = document.querySelector("header h1::after");
+const termDollar =
+  '<span class="terminal-green"><i class="fas fa-dollar-sign"></i><i class="fas fa-angle-right"></i></span>';
+headerH1.innerHTML += termDollar;
+const name = "Christian Foster";
+for (let i = 0; i < name.length; i++) {
+  setDelay(i, name);
 }
 
-function setDelay(i,name) {
-    setTimeout(function() {  
-        let htmlSpan = "<span>" + name[i] + "</span>";
-        headerH1.innerHTML += htmlSpan;
-    },i*100)
+function setDelay(i, name) {
+  setTimeout(function () {
+    let htmlSpan = "<span>" + name[i] + "</span>";
+    headerH1.innerHTML += htmlSpan;
+  }, i * 100);
 }
